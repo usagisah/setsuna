20ad1cda9d6e719e42f37139f9c25f5205ef5ad6
usagisah <usagisah@163.com>
2022-10-13 16:00:10 +0800

    fix Top level<children/>error reports

--- packages/setsuna/src/patch/patchOptions/component/children.js
+++ packages/setsuna/src/patch/patchOptions/component/children.js
@@ -2,7 +2,9 @@ import { patchFragment } from "../fragment/patchFragment"
 
 export function patchSlot(context) {
   const { parentComponent, VNode, slot } = context.parentComponent
-  parentComponent.deps.add(VNode.update)
   context.newVNode.children = slot
+  if (parentComponent) {
+    parentComponent.deps.add(VNode.update)
+  }
   return patchFragment(context)
 }
